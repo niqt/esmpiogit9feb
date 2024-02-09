88768931d31d023ea49f3169d0d37ed70422f3f5
Nicola De Filippo <nicola@nicoladefilippo.it>
2024-02-09 11:49:59 +0100

    Initial Commit

---
+++ esmpiogit9feb/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  esmpiogit9feb
+//
+//  Created by nicola de filippo on 09/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ esmpiogit9feb/esmpiogit9febApp.swift
@@ -0,0 +1,17 @@
+//
+//  esmpiogit9febApp.swift
+//  esmpiogit9feb
+//
+//  Created by nicola de filippo on 09/02/24.
+//
+
+import SwiftUI
+
+@main
+struct esmpiogit9febApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
